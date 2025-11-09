e31de1ef0d235781c468a2a9dd9920499e0bc1d8
Olman-dev <olmanzapata22@gmail.com>
2025-11-08 22:02:02 -0500

    Cambios

---
+++ Front/src/main/java/CryptoFront/Front/FrontApplication.java
@@ -0,0 +1,18 @@
+package CryptoFront.Front;
+
+import CryptoFront.Front.Controller.UsuarioController;
+import CryptoFront.Front.Service.Impl.ApiServiceImpl;
+import CryptoFront.Front.Service.Impl.UsuarioServiceImpl;
+import CryptoFront.Front.Ui.MenuInicio;
+
+public class FrontApplication {
+    public static void main(String[] args) {
+        String baseUrl = "http://localhost:8080";
+
+        ApiServiceImpl apiService = new ApiServiceImpl(baseUrl);
+        UsuarioServiceImpl usuarioService = new UsuarioServiceImpl(apiService);
+        UsuarioController usuarioController = new UsuarioController(usuarioService);
+
+        javax.swing.SwingUtilities.invokeLater(() -> new MenuInicio(usuarioController));
+    }
+}
